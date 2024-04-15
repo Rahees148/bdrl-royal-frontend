ecad906525e599ea7f56cb51493be24c7c1ee6c4
Rayees Nadukkandy <rayees.nadukkandy@tdcx.com>
2024-04-15 20:19:32 +0800

    image text section added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import HeroBanner from '../components/hero-banner';
 import BookAnAppointment from '../components/book-an-appointment';
 import CentresOfExcellence from '../components/centres-of-excellence';
 import { Fade } from 'react-awesome-reveal';
-
+import doctorImage from '../images/image-1.png'
 import { Content } from '../graphql/home';
 import SlidingCard from '../components/sliding-card';
 import PatientTestimonials from '../components/patient-testimonials';
@@ -24,8 +24,18 @@ const IndexPage = () => {
             <SlidingCard data={pageData.sliding_card} />
             <ExceptionalCare data={pageData.exceptional_care} />
             {/* Centres Of Excellence component */}
-            <section className="bg-white py-12">
-                <div className="pageWrapper">test</div>
+            <section className="bg-gradient-golden">
+                <div className='bg-white absolute top-0 left-0 w-[50%] h-[100%] z-0'></div>
+                <div className="pageWrapper flex relative z-30 ">
+                    <div className='w-[60%] bg-white pt-8'>
+                        test
+                        
+                    </div>
+                    <div className='w-[40%] clipContainer pt-8'>
+                        <img src={doctorImage} alt='test' />
+                        <div className='bg-white clip'></div>
+                    </div>
+                </div>
             </section>
             <PatientTestimonials titleDescription={pageData.testimonials_title} />
         </Layout>
