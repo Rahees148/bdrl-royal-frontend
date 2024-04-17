a732232421dcaed0cb49b0eebbf75c7bd14a61ee
Rayees Nadukkandy <rayees.nadukkandy@tdcx.com>
2024-04-18 00:21:18 +0800

    responsive changes

--- src/components/global/title-description/index.js
+++ src/components/global/title-description/index.js
@@ -5,45 +5,43 @@ import * as style from './title-description.module.scss';
 function TitleDescription({ data }) {
     return (
         <>
-        {data.variant === 'details' ?(
-            <>
-            <div
-            className={classNames(
-                style.SectionTitleDetails,
-                data.theme ? style[data.theme] : style['green'],
-            )}
-        >
-            <h3  dangerouslySetInnerHTML={{ __html: data.title }} />
-            <div className={style.spacer}></div>
-            <div className={classNames(style.description)}
-                dangerouslySetInnerHTML={{ __html: data.description }}
-            />
-        </div>
-        <div className={style.curve}></div>
-        </>
-        ):(
-        <div
-            className={classNames(
-                style.SectionTitle,
-                data.theme ? style[data.theme] : style['green'],
-                'flex items-end justify-between',
-            )}
-        >
-            <div>
-                <h3>{data.title}</h3>
-                <div className={classNames(style.SectionTagline, 'font-normal')}>
-                    {data.description}
+            {data.variant === 'details' ? (
+                <div
+                    className={classNames(
+                        style.SectionTitleDetails,
+                        data.theme ? style[data.theme] : style['green'],
+                    )}
+                >
+                    <h3 dangerouslySetInnerHTML={{ __html: data.title }} />
+                    <div className={style.spacer}></div>
+                    <div
+                        className={classNames(style.description)}
+                        dangerouslySetInnerHTML={{ __html: data.description }}
+                    />
                 </div>
-            </div>
-            {data.button_label &&
-            <a className={classNames('moreBtn', data.theme)} href={data.button_link} >{data.button_label}</a>
-        }
-        </div>
-        )}
-        
+            ) : (
+                <div
+                    className={classNames(
+                        style.SectionTitle,
+                        data.theme ? style[data.theme] : style['green'],
+                        'flex items-end justify-between',
+                    )}
+                >
+                    <div>
+                        <h2>{data.title}</h2>
+                        <div className={classNames(style.SectionTagline, 'font-normal')}>
+                            {data.description}
+                        </div>
+                    </div>
+                    {data.button_label && (
+                        <a className={classNames('moreBtn', data.theme)} href={data.button_link}>
+                            {data.button_label}
+                        </a>
+                    )}
+                </div>
+            )}
         </>
-    
-  )
+    );
 }
 
 export default TitleDescription;
