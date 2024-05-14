3abd154978c21e90d229f69a7c6d9b9c9137881b
dhishildas <dhishildas@gmail.com>
2024-05-14 20:40:51 +0100

    contact page updated

--- src/components/contact-details/index.js
+++ src/components/contact-details/index.js
@@ -4,13 +4,15 @@ import classNames from 'classnames'
 import LeadershipForm from '../form/leadership-form'
 import { Parallax } from 'react-scroll-parallax'
 import ParallaxElement from '../parallax-element'
+import EmailIcon from '../../images/icons/mail-icon.svg';
+import CallIcon from '../../images/icons/call-icon.svg';
 
 function ContactDetails({pageData}) {
   return (
     <div className={style['contactDetails']}>
     <section className='py-[80px] relative z-10'>
         <div className={classNames('pageWrapper')}>
-            <span className='button gold'>{pageData.title}</span>
+            <span className={classNames(style.ContactTag,' gold')}>{pageData.title}</span>
             <h2 dangerouslySetInnerHTML={{__html: pageData.banner_title?.data.banner_title}} /> 
         </div>
       </section>
@@ -22,14 +24,14 @@ function ContactDetails({pageData}) {
             <ParallaxElement variant='circle' style={{left:'60%', top:'-300px'}} />
         </Parallax>
        </div>
-      <section className='pt-[66px] pb-[81px] bg-bdrlGray '>
-        <div className='pageWrapper grid grid-cols-2 gap-8 relative z-10' >
+      <section className='pt-[45px] sm:pt-[66px] pb-[45px] sm:pb-[81px] bg-bdrlGray '>
+        <div className='pageWrapper flex flex-col sm:grid grid-cols-2 gap-[34px] sm:gap-[34px] relative z-10' >
             <div className='p-[45px] bg-white rounded-5'>
-                <ul>
-                    <li>{pageData.email}</li>
-                    <li>{pageData.number}</li>
+                <ul className={classNames(style.CallDetails)}>
+                    <li><span><img src={EmailIcon} alt='Email'/></span>{pageData.email}</li>
+                    <li><span><img src={CallIcon} alt='Call'/></span>{pageData.number}</li>
                 </ul>
-                <div>
+                <div className={classNames(style.ContactFollowUs)}>
                     <h3>{pageData.follow_us_label}</h3>
                     <p>{pageData.follow_us_description}</p>
                     <ul className='flex gap-4'>
@@ -50,11 +52,11 @@ function ContactDetails({pageData}) {
             </div>
         </div>
       </section>
-      <section className='py-[80px]'>
-      <div className='pageWrapper grid grid-cols-2 gap-8 items-center'>
-            <div className=' bg-white rounded-5 h-[544px] overflow-hidden' dangerouslySetInnerHTML={{__html: pageData.location_map}} />
-            <div>
-                <span className=''>{pageData.location_subtitle}</span>
+      <section className='py-[45px] sm:py-[80px]'>
+      <div className='pageWrapper sm:grid grid-cols-2 gap-[60px] sm:items-center flex flex-col items-stretch'>
+            <div className=' bg-white rounded-5 h-[544px] overflow-hidden order-last sm:order-first' dangerouslySetInnerHTML={{__html: pageData.location_map}} />
+            <div className={classNames(style.ContactLocation)}>
+                <span className={classNames(style.ContactLocationSubtitle)}>{pageData.location_subtitle}</span>
                 <h4>{pageData.location_title}</h4>
 
                 <h5>{pageData.address_title}</h5>
